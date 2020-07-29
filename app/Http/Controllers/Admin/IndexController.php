3c9214bb6082db13a559300b10570ce3dec330da
wumenglin <1501113246@qq.com>
2020-07-30 07:34:58 +0800

    支付宝

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
+use App\Model\Token;
 
 class indexController extends Controller
 {
@@ -18,7 +19,12 @@ class indexController extends Controller
         $pwd=request()->post("pwd");
         $url="http://www.api.com/index/login?username=".$username."&email=".$email."&pwd=".$pwd;
         $res=file_get_contents($url);
-        echo $res;
+        echo $res;echo "<hr>";
+        $res=json_decode($res,true);
+        $token=$res["data"]["token"];
+        if($res["errno"]==0){
+            return redirect("http://www.lin.com/admin/user?token=".$token);
+        }
     }
     //注册
     public function reg(){
@@ -31,6 +37,10 @@ class indexController extends Controller
         $url="http://www.api.com/index/reg?username=".$username."&email=".$email."&pwd=".$pwd;
         $res=file_get_contents($url);
         echo $res;
+        $res=json_decode($res,true);
+        if($res["error"]==0){
+            return redirect("http://www.lin.com/admin/login");
+        }
     }
     //用户中心
     public function user(){
@@ -42,6 +52,18 @@ class indexController extends Controller
             ];
             return $response;
         }
+
+        $data=Token::where(["token"=>$token])->first();
+        if(!$data){
+            $response=[
+                "error"=>"40005",
+                "msg"=>"请输入正确得token"
+            ];
+            return $response;
+        }
+//        $url="http://www.api.com/index/user";
+//        $res=file_get_contents($url);
+//        echo $res;
         return view("admin/index/user");
     }
 }
