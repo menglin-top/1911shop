58975330cc14063434ccb65cf45a68667fd17158
wumenglin <1501113246@qq.com>
2020-08-01 15:05:54 +0800

    github

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,8 +8,14 @@ class AuthController extends Controller
 {
     public function auth(){
         $id="6d28866c8aa66430e361";
-        $url="https://github.com/login/oauth/authorize?id=".$id;
+        $url="https://github.com/login/oauth/authorize?client_id=".$id;
         $res=file_get_contents($url);
         echo $res;
+    }
+    public function auth2($code){
+        $client_id="6d28866c8aa66430e361";
+        $client_secret="5ea5245a8318c7f460a401afc8eaeb001c61b487";
+        $url="https://github.com/login/oauth/access_token?client_id=".$client_id."&client_secret=".$client_secret."&code=".$code;
+
     }
 }
